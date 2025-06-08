1b8d76b56af6aadf05da1c678833ae74242fb86a
Dasun Thamash <tunathamash@gmail.com>
2025-06-08 15:07:38 +0530

    update new version

--- hotel-reservation-system/resources/views/emails/reservation-confirmation.blade.php
+++
@@ -1,7 +0,0 @@
-<p>Dear {{ $reservation->user->name }},</p>
-<p>Your {{ $reservation->is_suite ? 'suite' : 'room' }} reservation at {{ $reservation->branch->name }} has been confirmed.</p>
-<p>Check-in: {{ $reservation->check_in_date->format('Y-m-d') }}</p>
-<p>Check-out: {{ $reservation->check_out_date->format('Y-m-d') }}</p>
-<p>Room Type: {{ $reservation->roomType->name }}</p>
-<p>Occupants: {{ $reservation->number_of_occupants }}</p>
-<p>Thank you for choosing us!</p>
